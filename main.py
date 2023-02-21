1b0e58a40af99e5cecf6222eefe8c45fd058d968
Caoimhín Laoide-Kemp <c.laoide-kemp@epcc.ed.ac.uk>
2023-02-21 08:54:41 +0000

    Functionality now complete

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import requests, json
 import pandas as pd
 import csv
 
-def construct_draft_teams():
+def construct_draft_teams(managers):
     draft_teams = {}
-    for manager in ["Brian","Caoimhín","Niamh","Seán","Violet"]:
+    for manager in managers:
         add_draft_team(draft_teams,manager)
     return draft_teams
 
@@ -24,9 +24,10 @@ def add_player(draft_teams,manager,position,player,id_num,outfield_sub):
     if(outfield_sub!=0):
         draft_teams[manager]["subs"][outfield_sub]={"name":player,"position":position,"id":id_num,"score":0,"minutes":0}
     else:
-        draft_teams[manager][position][player]=id_num
-        draft_teams[manager][position]["score"]=0
-        draft_teams[manager][position]["minutes"]=0
+        draft_teams[manager][position][player]={}
+        draft_teams[manager][position][player]["id"]=id_num
+        draft_teams[manager][position][player]["score"]=0
+        draft_teams[manager][position][player]["minutes"]=0
 
 def read_player_csv(filename):
     with open(filename, newline='') as f:
@@ -63,10 +64,81 @@ def get_gameweek_history(player_id):
     player_df = pd.json_normalize(r['history'])
     return player_df
 
+def add_gameweek_data(draft_teams,gameweek,managers):
+    for manager in managers:
+        for position in ["start_gkp","sub_gkp","def","mid","fwd","subs"]:
+            for player in draft_teams[manager][position]:
+                player_gameweek_history = get_gameweek_history(draft_teams[manager][position][player]["id"])
+                draft_teams[manager][position][player]["score"] = player_gameweek_history.loc[player_gameweek_history['round'] == gameweek, 'total_points'].sum()
+                draft_teams[manager][position][player]["minutes"] = player_gameweek_history.loc[player_gameweek_history['round'] == gameweek, 'minutes'].sum()
+
+def get_score(team):
+    starting_positions = ["def","mid","fwd","start_gkp"]
+    positions = ["start_gkp","sub_gkp","def","mid","fwd","subs"]
+    min_formation = [3,2,1]
+    score = get_initial_score(team)
+    formation = get_formation(team)
+    num_subs = 0
+    for position in starting_positions:
+        for player in team[position]:
+            if team[position][player]["minutes"] == 0:
+                if position == "start_gkp":
+                    for player in team["sub_gkp"]:
+                        score = score + team["sub_gkp"][player]["score"]
+                else:
+                    formation[starting_positions.index(position) - 1] = formation[starting_positions.index(position)]-1
+                    num_subs = num_subs + 1
+
+    subs_required = [0,0,0]
+    for index, position in enumerate(subs_required):
+        if min_formation[index] > formation[index]:
+            subs_required[index] = min_formation[index] - formation[index]
+
+    while num_subs > 0:
+        sub_not_made = True
+        if sum(subs_required) > 0:
+            for sub in range(1,4):
+                if team["subs"][sub]["minutes"] > 0 and subs_required[starting_positions.index(team["subs"][sub]["position"])] > 0:
+                    score = score + team["subs"][sub]["score"]
+                    num_subs = num_subs - 1
+                    team["subs"][sub]["minutes"] = 0
+                    subs_required[starting_positions.index(team["subs"][sub]["position"])] = subs_required[starting_positions.index(team["subs"][sub]["position"])] - 1
+                    sub_not_made = False
+                    break
+        else:
+            for sub in range(1,4):
+                if team["subs"][sub]["minutes"] > 0:
+                    score = score + team["subs"][sub]["score"]
+                    num_subs = num_subs - 1
+                    team["subs"][sub]["minutes"] = 0
+                    sub_not_made = False
+                    break
+        if sub_not_made:
+            break
+
+    return score
+    
+
+def get_initial_score(team):
+    starting_positions = ["start_gkp","def","mid","fwd"]
+    initial_score = 0
+    for position in starting_positions:
+        for player in team[position]:
+            initial_score = initial_score + team[position][player]["score"]
+    return initial_score
+
+def get_formation(team):
+    formation = [len(team["def"]),len(team["mid"]),len(team["fwd"])]
+    return formation
+
 if __name__ == "__main__":
-    draft_teams = construct_draft_teams()
+    managers = ["Brian","Caoimhín","Niamh","Seán","Violet"]
+    draft_teams = construct_draft_teams(managers)
     player_data = read_player_csv("drafted_players_2223.csv")
     for player in player_data:
         add_player(draft_teams,player[0],player[1],player[2],int(player[3]),int(player[4]))
-    test = get_gameweek_history(478)
-    print(test[['round','minutes','total_points']])
+    for gameweek in range(25, 26):
+        print("Gameweek: ",gameweek)
+        add_gameweek_data(draft_teams,gameweek,managers)
+        for manager in managers:
+            print(get_score(draft_teams[manager]))
